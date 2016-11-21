64952dfb49f677754aecfcb394869eef4e4af143
Erlend Oftedal <erlend@oftedal.no>
2016-11-21 23:27:12 +0100

    sponsor links

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,16 @@ window.addEventListener("load", function() {
 	function query(e, q) {
 		return Array.prototype.slice.call(e.querySelectorAll(q));
 	}
-	var uls = query(document, "ul.sponsors");
-	for (var i in uls) {
-		var ul = uls[i];
-		var lis = query(ul, "li");
-		for (var j in lis) ul.removeChild(lis[j]);
-		while(lis.length > 0) {
-			var ix = Math.floor(Math.random() * lis.length);
-			var li = lis[ix];
-			lis = lis.slice(0,ix).concat(lis.slice(ix+1,lis.length));
-			ul.appendChild(li);
+	var sponsors = query(document, "div.sponsors");
+	for (var i in sponsors) {
+		var container = sponsors[i];
+		var links = query(container, "a");
+		for (var j in links) container.removeChild(links[j]);
+		while(links.length > 0) {
+			var ix = Math.floor(Math.random() * links.length);
+			var a = links[ix];
+			links = links.slice(0,ix).concat(links.slice(ix+1,links.length));
+			container.appendChild(a);
 		}
 	}
 }, false);
